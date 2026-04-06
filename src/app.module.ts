a0b677ca076d496b729bf5574aab644951fe7393
Kyryll Vitoshkin <56431973+Cheastear@users.noreply.github.com>
2026-04-06 20:55:59 +0200

    typeorm initialisation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './data.source';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/data.source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import 'dotenv/config';
+
+const configService = new ConfigService();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST')!,
+  port: configService.get<number>('DB_PORT')!,
+  username: configService.get<string>('DB_USER')!,
+  password: configService.get<string>('DB_PASSWORD')!,
+  database: configService.get<string>('DB_NAME')!,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
+});
