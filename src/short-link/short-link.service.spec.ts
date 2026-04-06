adf8f04de59e48543d78bcf1ac96befd7f36dcf5
Kyryll Vitoshkin <56431973+Cheastear@users.noreply.github.com>
2026-04-06 22:21:52 +0200

    short link tests

--- src/short-link/short-link.service.spec.ts
+++ src/short-link/short-link.service.spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShortLinkService } from './short-link.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '../data.source';
+import { ShortLink } from './entity/short-link.entity';
+
+jest.mock('nanoid/non-secure', () => ({
+  nanoid: jest.fn(() => 'TESTCODE'),
+}));
 
 describe('ShortLinkService', () => {
   let service: ShortLinkService;
 
+  let link: ShortLink;
+
+  const testLink = 'test.com/superlink';
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(AppDataSource.options),
+        TypeOrmModule.forFeature([ShortLink]),
+      ],
       providers: [ShortLinkService],
     }).compile();
 
@@ -14,5 +29,40 @@ describe('ShortLinkService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create link', async () => {
+    link = await service.create({ originalUrl: testLink });
+
+    expect(link.originalUrl).toBe(testLink);
+  });
+
+  it('should return link entity', async () => {
+    const linkEntity = await service.getLink({ code: link.code });
+
+    expect(linkEntity.id).toBe(link.id);
+    expect(linkEntity.code).toBe(link.code);
+    expect(linkEntity.originalUrl).toBe(link.originalUrl);
+  });
+
+  it('should increase clicks link', async () => {
+    const before = await service.getLink({ code: link.code });
+    await service.incrementClicks({ code: link.code });
+    const after = await service.getLink({ code: link.code });
+
+    expect(before.originalUrl).toBe(after.originalUrl);
+    expect(before.clicks).toBe(link.clicks);
+    expect(before.clicks + 1).toBe(after.clicks);
+
+    link = after;
+  });
+
+  it('should delete link', async () => {
+    const result = await service.deleteLink({ code: link.code });
+
+    const isExist = await service.getLink({ id: link.id }).catch(() => false);
+
+    expect(result.affected).toBe(1);
+    expect(isExist).toBe(false);
   });
 });
--- src/short-link/short-link.service.ts
+++ src/short-link/short-link.service.ts
@@ -8,7 +8,7 @@ import { CreateShortLinkDto } from './dto/create-short-link.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ShortLink } from './entity/short-link.entity';
 import { LessThan, Repository } from 'typeorm';
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid/non-secure';
 
 @Injectable()
 export class ShortLinkService {
@@ -22,7 +22,7 @@ export class ShortLinkService {
     let exists: ShortLink | null;
 
     do {
-      code = nanoid(6);
+      code = nanoid(8);
       exists = await this.repo.findOne({ where: { code } });
     } while (exists);
 
