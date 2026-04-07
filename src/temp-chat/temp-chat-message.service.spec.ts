e7b3303faf5dec921a5a4406da27da2e5494bb39
Kyryll Vitoshkin <56431973+Cheastear@users.noreply.github.com>
2026-04-07 20:24:51 +0200

    fix: tests

--- src/temp-chat/temp-chat-message.service.spec.ts
+++ src/temp-chat/temp-chat-message.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TempChatMessageService } from './temp-chat-message.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '../data.source';
+import { TempChatMessage } from './entities/temp-chat-message.entity';
 
 describe('TempChatMessageService', () => {
   let service: TempChatMessageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(AppDataSource.options),
+        TypeOrmModule.forFeature([TempChatMessage]),
+      ],
       providers: [TempChatMessageService],
     }).compile();
 
--- src/temp-chat/temp-chat.service.spec.ts
+++ src/temp-chat/temp-chat.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TempChatService } from './temp-chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '../data.source';
+import { TempChat } from './entities/temp-chat.entity';
 
 describe('TempChatService', () => {
   let service: TempChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(AppDataSource.options),
+        TypeOrmModule.forFeature([TempChat]),
+      ],
       providers: [TempChatService],
     }).compile();
 
